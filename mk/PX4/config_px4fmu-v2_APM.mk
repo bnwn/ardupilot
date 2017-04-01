5e6cac8ef95becc304d137006fc2aae1d2c5bfe5
bnwn <bnwn_bnwn@126.com>
2017-04-01 11:10:13 +0800

    fix

--- mk/PX4/config_px4fmu-v2_APM.mk
+++ mk/PX4/config_px4fmu-v2_APM.mk
@@ -5,8 +5,5 @@ include $(SKETCHBOOK)/mk/PX4/px4_common.mk
 
 MODULES		+= drivers/boards/px4fmu-v2
 MODULES		+= drivers/pwm_input
-#MODULES     += drivers/hz16wa
-#MODULES		+= drivers/m006
-MODULES         += lib/mathlib
 MODULES		+= drivers/px4io
 MODULES		+= modules/uavcan
